50dbb0bd23da88eebdde50617adbf9ee23e96061
kotmw0701 <supurasyu@hotmail.co.jp>
2018-11-06 23:16:59 +0900

    画像管理クラスの追加

--- DesktopMascots/ImagePair.cs
+++ DesktopMascots/ImagePair.cs
@@ -8,5 +8,12 @@ using System.Windows.Media;
 
 namespace DesktopMascots {
     class ImagePair {
+        public MascotImage LeftImage { get; }
+        public MascotImage RightImage { get; }
+
+        public ImagePair(MascotImage leftImage, MascotImage rightImage) {
+            LeftImage = leftImage;
+            RightImage = rightImage;
+        }
     }
 }
---
+++ DesktopMascots/MascotImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DesktopMascots {
+    class MascotImage {
+        public ImageBrush Image { get; }
+        public Point Center { get; }
+        public double Height { get; }
+        public double Width { get; }
+
+        public MascotImage(BitmapImage image, Point center) {
+            Image = new ImageBrush { ImageSource = image };
+            Center = center;
+            Height = image.Height;
+            Width = image.Width;
+        }
+    }
+}
